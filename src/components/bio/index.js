6b7a4a1a8c2a09126aadef88d5306cba487e272c
Amber Brown <amberbrown07@hotmail.co.uk>
2023-03-18 20:54:03 +0000

    initial-changes

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -1,20 +1,14 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import "./style.css"
 
 const Bio = () => {
   return (
-    <div className="bio__container">
-      <div className="bio__text-container">
-        <h2>Hi, I'm Amber Brown</h2>
-        <p className="bio__subheading">A UK based full stack javascript developer</p>
-      </div>
+    <div>
+      <h2 className="bio__name-text"><span className="bio__big-text">HEY! </span><span>I'm Amber,</span> <span className="text-light">a</span></h2>
+      <h1>SOFTWARE ENGINEER<br />
+      <span className="text-light">based in </span><b>LONDON</b></h1>
+      <p className="colour-secondary">Javascript / typescript engineer with experience in building<br />solutions for mobile and web</p>
+      <button>Find out more</button>
     </div>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 
-class Header extends React.Component {
-  render() {
+const Header = () => {
     return (
       <header className="main-header">
-        <div class="main-header__background">
+        <div className="main-header__background">
           <svg
             width="1187"
             height="395"
@@ -15,7 +14,7 @@ class Header extends React.Component {
           >
             <path
               d="M1187 0H0C19.1733 12.9508 106.325 70.3045 189.99 99.9063C273.655 129.508 402.639 154.485 545.567 163.735C688.495 172.986 786.104 174.836 936.004 227.564C1055.92 269.747 1153.3 356.764 1187 395V0Z"
-              fill="rgb(208, 199, 186)"
+              fill="#0e0d0c"
             />
           </svg>
         </div>
@@ -34,7 +33,6 @@ class Header extends React.Component {
         </div>
       </header>
     )
-  }
 }
 
 export default Header
